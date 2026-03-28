b06c219d9960cc02c7d08bb108847610d4cb16db
tho071206-ther <tho071206@gmail.com>
2026-03-28 13:23:35 +0700

    Initial commit with source code

---
+++ HeDieuHanh_DeTai16/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HeDieuHanh_DeTai16
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ HeDieuHanh_DeTai16/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeDieuHanh_Detai16.ViewModel
+{
+    internal class BaseViewModel
+    {
+    }
+}
