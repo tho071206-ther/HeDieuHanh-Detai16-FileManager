0c1767b988dd12dd4e2ce7797eb624386cac5d2b
tho071206-ther <tho071206@gmail.com>
2026-04-01 11:35:02 +0700

    Don dep cau truc thu muc va loai bo .vs

--- HeDieuHanh_DeTai16/Views/NavigationView.xaml.cs
+++ HeDieuHanh_DeTai16/Views/NavigationView.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace HeDieuHanh_DeTai16.Views
@@ -17,7 +18,7 @@ namespace HeDieuHanh_DeTai16.Views
     /// <summary>
     /// Interaction logic for NavigationView.xaml
     /// </summary>
-    public partial class NavigationView : Window
+    public partial class NavigationView : UserControl
     {
         public NavigationView()
         {
--- HeDieuHanh_DeTai16/Views/SearchView.xaml.cs
+++ HeDieuHanh_DeTai16/Views/SearchView.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace HeDieuHanh_DeTai16.Views
@@ -17,7 +18,7 @@ namespace HeDieuHanh_DeTai16.Views
     /// <summary>
     /// Interaction logic for SearchView.xaml
     /// </summary>
-    public partial class SearchView : Window
+    public partial class SearchView : UserControl
     {
         public SearchView()
         {
